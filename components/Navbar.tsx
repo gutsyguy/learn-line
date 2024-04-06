0ba59ce2e79ed61551a0b6528f0937541c0054da
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-04-06 13:53:53 -0700

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,22 @@
 import React from "react"
 import Link from "next/link"
-import Image from "next/image"
 
 const Navbar = () => {
 return(
-<div className="flex flex-row items-center space-x-6 mt-2 mb-2 top-0 font-semibold text-lg py-3" > 
-        <div className="text-gray-800 space-x-2 left-0"> 
-         <Link href="/"> 
-            <Image
-            src="/profile.png"
-            width={90}
-            height={90}
-            alt="Our Logo!"
-    /></Link>
+<div className="flex fixed flex-row space-x-4 mt-2 mb-2 top-0 font-semibold text-lg py-3 justify-center h-screen" > 
+     <div className="text-white absolute left-0"> 
          <Link href="/">LearnLine</Link>
+    </div>
+    <div className="absolute right-0 ">     
+        <div className="text-white"> 
+            <Link href="/">About</Link>
         </div>
-        <div className="text-gray-800"> 
-         <Link href="/">Home</Link>
-        </div>
-        <div className="text-gray-800"> 
-         <Link href="/">About</Link>
-        </div>
-        <div className="text-gray-800"> 
-         <Link href="/Welcome">Sign Up</Link>
+        <div className=" bg-rose-500 text-white px-4 py-2 my-1 rounded-md"> 
+            <Link href="/">Sign Up</Link>
         </div>
     </div>
+</div>
  )
 }
+
+export default Navbar
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
+    <main>
+      
      
+    <div className="flex static">
+      <Navbar />
+    </div>
+
     </main>
   );
 }
