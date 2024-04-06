2a423f4840c1925667eef10cb0ec5ae9555c07fe
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-04-06 14:33:45 -0700

    ui update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,21 +2,23 @@ import React from "react"
 import Link from "next/link"
 
 const Navbar = () => {
-return(
-<div className="flex fixed flex-row space-x-4 mt-2 mb-2 top-0 font-semibold text-lg py-3 justify-center h-screen" > 
-     <div className="text-white absolute left-0"> 
-         <Link href="/">LearnLine</Link>
-    </div>
-    <div className="absolute right-0 ">     
-        <div className="text-white"> 
-            <Link href="/">About</Link>
+    return (
+        <div className="flex justify-center mt-2 top-0 w-full">
+          <div className="flex relative flex-row space-x-4 font-semibold text-lg">
+            <div className="text-white">
+              <Link href="/">LearnLine</Link>
+            </div>
+            <div className="flex flex-row items-center space-x-4">
+              <div className="text-white">
+                <Link href="/">About</Link>
+              </div>
+              <div className="bg-rose-500 text-white px-4 py-2 my-1 rounded-md">
+                <Link href="/">Sign Up</Link>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className=" bg-rose-500 text-white px-4 py-2 my-1 rounded-md"> 
-            <Link href="/">Sign Up</Link>
-        </div>
-    </div>
-</div>
- )
-}
-
+      );
+    };
+    
 export default Navbar
