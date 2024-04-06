47cb88851ffe289d0390c5c17abf64758d34aeae
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-04-06 15:33:00 -0700

    ejid

--- components/About.tsx
+++ components/About.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const About = () => {
     return(
         <div className="flex flex-row my-2 mx-2 "> 
-            <div className=""> 
+            <div className="bg-"> 
                 Allowing students to take the best steps torwards their future
             </div>
             <div className=""> 
