fd0d058ce9d38bea1dca1da6b25c567e1c05ba68
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-04-07 04:15:48 -0700

    new widget (not useable)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,14 +6,14 @@ const Navbar = () => {
         <div className="flex justify-center pt-2 top-0 h-min-screen text-center ">
           <div className="flex relative flex-row space-x-4 font-semibold text-lg">
             <div className="flex flex-row items-center space-x-4 text-black">
-              <div className="bg-rose-500 text-white px-4 py-2 my-1 rounded-md">
-              <Link href="/">LearnLine</Link>
+              <div className="bg-rose-700 text-white px-4 py-2 my-1 rounded-md">
+              <Link href="/index">LearnLine</Link>
               </div>
-              <div className="bg-rose-500 text-white px-4 py-2 my-1 rounded-md">
+              <div className="bg-rose-700 text-white px-4 py-2 my-1 rounded-md">
                 <Link href="/">About</Link>
               </div>
-              <div className="bg-rose-500 text-white px-4 py-2 my-1 rounded-md">
-                <Link href="/home">Sign Up</Link>
+              <div className="bg-rose-700 text-white px-4 py-2 my-1 rounded-md">
+                <Link href="/home">Sign In</Link>
               </div>
             </div>
           </div>
---
+++ components/UserWidget.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const UserWidget = () => {
+    return(
+        <div className="flex flex-col my-2 mx-2 space-x-4 space-y-4 items-center h-screen w-screen">
+            <div className="line-clamp-one bg-gradient-to-r from-amber-200 to-amber-700 rounded-md text-black max-w-15 py-2 px-2 align-middle max-h-15 leading-loose font-semibold text-center font-sans">
+                <div className="absolute justify-items-center"> 
+                <Image
+                src="/info.svg"
+                width={120}
+                height={120}
+                alt="Info Symbol"
+                />
+                </div>
+                <div className="font-bold">
+                 Congratulations!
+                </div>
+                 You've completed our form and now are ready for the rest of your life
+            </div>
+             <button className="flex basis-3 static bg-amber-200 text-black px-2 py-0.5 my-1 rounded-full items-center text-lg">  
+                <Link href="/">Ready to see your Profile?</Link>
+             </button>
+        </div>
+    )
+}
+
+export default UserWidget
