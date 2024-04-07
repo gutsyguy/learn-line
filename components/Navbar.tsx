9c7d33c42b3de68049a0584cdc49c7dd618dc5cb
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-04-06 18:25:37 -0700

    new

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,8 +4,7 @@ import Link from "next/link"
 const Navbar = () => {
     return (
         <div className="flex justify-center pt-2 top-0 w-full text-center bg-black ">
-          <div className="flex relative flex-row space-x-4 font-semibold text-lg">
-            
+          <div className="flex relative flex-row space-x-4 font-semibold text-lg">   
             <div className="flex flex-row items-center space-x-4">
               <div className="text-white">
               <Link href="/">LearnLine</Link>
