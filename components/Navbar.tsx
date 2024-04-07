68b37db383c19cb21a21d61c6609e4098d8b71df
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-04-07 05:52:32 -0700

    ay

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
                 <Link href="/">About</Link>
               </div>
               <div className="bg-rose-700 text-white px-4 py-2 my-1 rounded-md">
-                <Link href="/home">Sign In</Link>
+                <Link href="/Profile">Sign In</Link>
               </div>
             </div>
           </div>
--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -4,16 +4,8 @@ import Link from 'next/link'
 
 const UserProfile = () => {
     return(
-        <div className="flex flex-col my-2 mx-2 space-x-4 space-y-4 items-center h-screen w-screen">
-            <div className="flex relative"> 
-             <Image
-             src="/user.svg"
-             width={120}
-             height={120}
-             alt="Our Logo "
-             />
-            </div> 
-            <div className="flex-row line-clamp-one bg-amber-200 rounded-md text-black max-w-15 py-2 px-2 align-middle max-h-15 leading-loose font-semibold text-center divide-y-2">
+        <div className="flex flex-col my-2 mx-2 space-x-4 space-y-4 items-center h-screen w-screen"> 
+            <div className="flex flex-col-reverse line-clamp-one rounded-md text-black max-w-15 py-2 px-2 align-middle max-h-15 leading-loose font-semibold text-center divide-y divide-y-reverse divide-black bg-amber-200">
                 <div className="font-bold">
                 Insert Name Here 
                 </div>
@@ -22,10 +14,16 @@ const UserProfile = () => {
                 Future career aspirations here
                 </div>
                 <div className="current course listings here">
-
                 </div>
-            </div>
-             
+                <div className="flex relative justify-center"> 
+                    <Image
+                    src="/user.svg"
+                    width={120}
+                    height={120}
+                    alt="Our Logo "
+                    />
+                </div>
+            </div>  
         </div>
     )
 }
