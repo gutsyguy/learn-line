f8f6702011ce5811c3af0e8c431c71ab61df52bc
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-04-07 02:41:32 -0700

    new wobbly widget included

--- components/Widget.tsx
+++ components/Widget.tsx
@@ -5,19 +5,21 @@ import Image from 'next/image'
 const Widget = () => {
     return(
         <div className="flex flex-col my-2 mx-2 space-x-4 space-y-4 items-center h-screen w-screen">
-            <div className="line-clamp-one bg-gradient-to-r from-amber-200 to-rose-700 rounded-md text-black max-w-15 py-2 px-2 align-middle max-h-15 leading-loose font-semibold text-center">
-                <div className="rounded-md"> 
+            <div className="line-clamp-one bg-gradient-to-r from-amber-200 to-rose-700 rounded-md text-black max-w-15 py-2 px-2 align-middle max-h-15 leading-loose font-semibold text-center font-sans">
+                <div className="rounded-md justify-items-center"> 
                 <Image
                 src="/danger.svg"
                 width={120}
                 height={120}
                 alt="Our Logo "
                 />
-                </div> 
+                </div>
+                <div className="space-y-2"> 
                 <div className="font-semibold">
                 It looks like you barely begun!
                 </div>
-                Begin to sculpt your ideal future today by taking a simple form now!
+                Begin to sculpt your ideal future today by taking our simple form now!
+                </div> 
             </div>
              <button className="flex basis-0.5 static bg-rose-500 text-black px-2 py-0.5 my-1 rounded-full items-center text-lg">  
                 <Link href="/">Get Started Now</Link>
--- pages/home.tsx
+++ pages/home.tsx
@@ -7,14 +7,17 @@ import Info from "@/components/Info";
 import Footer from "@/components/Footer";
 import Button from "@/components/Button";
 import Head from "next/head";
-import { ReactElement } from "react";
-import type { NextPage } from "next";
+import Widget from "@/components/widget";
+
 
 const Home = () => {
   return(
       <div className="flex flex-col bg-black items-center justify-center basis-3">
       <div className="flex absolute flex-col items-center justify-center -translate-y-15 translate-x-4">
         <Info />
+        <div className="flex relative"> 
+          <Widget />
+        </div>
       </div>
       <div className="bottom-5 fixed"> 
       <Footer />
