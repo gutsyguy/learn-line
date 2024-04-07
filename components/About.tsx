b0316c3feecff421d189a3c9313842bf13787835
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2024-04-06 17:00:25 -0700

    update

--- components/About.tsx
+++ components/About.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import Image from 'next/image'
-import favicon from '/public/favicon.ico'
+import learnlineImage from '../public/learnline.png';
 
 const About = () => {
     return(
-        <div className="flex flex-col my-2 mx-2 space-y-6"> 
+        <div className="flex relative flex-col my-2 mx-2 space-y-6"> 
             <div className="line-clamp-one bg-amber-100 rounded-md text-black max-w-13 py-2 px-2 align-middle max-h-12 leading-loose"> 
                 Allowing students to take the best steps torwards their future
             </div>
             <div className="rounded-md"> 
              <Image
-             src="/favicon.ico"
+             src="/learnline.png"
              width={120}
              height={120}
              alt="Our Logo "
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return(
-        <div className="bottom-0 my-2 mx-2 bg-white text-amber-100">
+        <div className="bottom-0 my-2 mx-2 bg-white text-amber-100 max-h-3 max-w-5 w-full">
             LearnLine © 2024
         </div>
     )
