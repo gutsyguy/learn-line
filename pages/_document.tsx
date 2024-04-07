ea13777f7136b2a7c6ea565b4663be1452345fc7
user <user@users-MacBook-Pro.local>
2024-04-06 20:54:32 -0700

    e

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,6 @@
 import Navbar from "@/components/Navbar";
 import About from "@/components/About";
 import Info from "@/components/Info";
-import Button from "@/components/button";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
---
+++ pages/quiz.tsx
@@ -0,0 +1,16 @@
+import Form from "@/components/Form";
+import { useState } from "react";
+
+const quiz = () =>{
+
+  return(
+    <div className="pt-12 text-3xl bg-[#FFFDCA] min-h-screen">
+      <h1 className="text-center ">Placement Quiz</h1> 
+
+      <Form /> 
+
+    </div>
+  )
+}
+
+export default quiz 
